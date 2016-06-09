420c4de524efe65a41aac5904021d0d362832977
Jakob Breuninger <jakob_breuninger@gmx.de>
2016-06-09 16:05:08 +0200

    fixed order

--- neuro_deep_planner/src/critic.py
+++ neuro_deep_planner/src/critic.py
@@ -62,7 +62,7 @@ class CriticNetwork:
             # L2 Regularization for all Variables
             self.regularization = 0
             for variable in self.critic_variables:
-                self.regularization = self.regularization + tf.nn.l2_loss(variable)
+                self.regularization += tf.nn.l2_loss(variable)
 
             self.regularization2 = tf.nn.l2_loss(self.critic_variables[0]) + \
                                    tf.nn.l2_loss(self.critic_variables[2]) + \
@@ -80,7 +80,7 @@ class CriticNetwork:
             # Define training optimizer
             self.y_input = tf.placeholder("float", [None, 1], name="y_input")
             self.td_error = tf.reduce_mean(tf.pow(self.Q_output-self.y_input, 2))
-            self.loss = self.td_error + self.regularization
+            self.loss = self.td_error + REGULARIZATION_DECAY * self.regularization
 
             tf.scalar_summary('td_error', tf.reduce_mean(self.td_error))
             tf.scalar_summary('regularization', tf.reduce_mean(self.regularization))
